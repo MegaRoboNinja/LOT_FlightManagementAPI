639bf0cc7ab1f2c0e3acf0f75ca1c7544c295a8f
MegaRoboNinja <131467705+MegaRoboNinja@users.noreply.github.com>
2024-04-20 00:58:55 +0200

    Updating Flights
    Added an asynchronous method that modifies flights already present in the database.

--- LOT_FlightManagementAPI/Controllers/FlightsController.cs
+++ LOT_FlightManagementAPI/Controllers/FlightsController.cs
@@ -2,6 +2,7 @@ using LOT_FlightManagementAPI.Data;
 using LOT_FlightManagementAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LOT_FlightManagementAPI.Controllers
 {
@@ -25,7 +26,7 @@ namespace LOT_FlightManagementAPI.Controllers
 
         // Get a specific flight by its ID
         [HttpGet("{id}")]
-        public async Task<ActionResult<Flight>> Get(UInt16 id)
+        public async Task<ActionResult<Flight>> Get(ushort id)
         {
             var flight = await _context.Flights.FindAsync(id);
             if (flight == null)
@@ -45,11 +46,29 @@ namespace LOT_FlightManagementAPI.Controllers
             return CreatedAtAction(nameof(Get), new {id = flight.FlightId}, flight);
         }
 
-        // PUT api/<FlightsController>/5
+        // Update/modify a flight
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(ushort id, [FromBody] Flight modifiedFlight)
         {
+            // Modification cannot change the flights id
+            // (could lead to two flights having the same id)
+            if (id != modifiedFlight.FlightId)
+                return BadRequest();
+
+            // Check if the flight to be modified even exists
+            if (FlightExists(id))
+                return NotFound();
+            
+            // Mark flight as modified
+            // (tells Entity Framework to update after async SaveChanges)
+            _context.Entry(modifiedFlight).State = EntityState.Modified;
+
+            // asynchronously save the changes
+            await _context.SaveChangesAsync();
+
+            return Ok();
         }
+        private bool FlightExists(ushort id) => _context.Flights.Any(f => f.FlightId == id);
 
         // DELETE api/<FlightsController>/5
         [HttpDelete("{id}")]
